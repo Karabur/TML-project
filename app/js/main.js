f3478a500a1a96556fcc6d2207276692fd3e4ed1
Karabur <anton.ulianov@gmail.com>
2013-12-13 10:56:57 +0200

    UTF support

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,14 @@
 'use strict';
+
+var tmpTest = "Title: Проверка примитива AddChoice\n" +
+    "TestName:;\n" +
+    "Difficulty: A1;\n" +
+    "FullTime: 0;\n" +
+    "Questions: 1;\n" +
+    "EndTitle.\n";
+//dfsdfываыва
 var codeEditor = new CodeMirror(document.body, {
-    value: "Lets start!\n",
-    mode:  "tml",
+    value: tmpTest,
+    mode: "tml",
     lineNumbers: true
 });
