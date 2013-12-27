dbce9a65b1edb4e7454dcb4520f150c472fde5e8
Robur <anton.ulianov@gmail.com>
2013-12-27 22:55:09 +0200

    more dynamic context

--- app/js/cm-directive.js
+++ app/js/cm-directive.js
@@ -2,21 +2,33 @@
 
 define(['directives'], function (directives) {
     directives.directive('cmEditor', function (sourceService, $rootScope) {
+        var editor;
+
+        function updateContext() {
+            var doc = editor.getDoc();
+            console.log(doc.getCursor());
+            var token = editor.getTokenAt(doc.getCursor());
+            console.log(token.state.context);
+            $rootScope.$apply(function () {
+                $rootScope.$broadcast('hintslist', token.state.context.getAutocompletionsList())
+            })
+        }
+
+        function clearContext() {
+            $rootScope.$apply(function () {
+                $rootScope.$broadcast('hintslist', [])
+            })
+        }
+
         function link(scope, element, attrs) {
-           var editor = window.editor = new CodeMirror(element[0], {
-                value: tmpTest,
+           editor = window.editor = new CodeMirror(element[0], {
                 mode: "tml",
                 lineNumbers: true
             });
 
-            editor.on('cursorActivity', function (editor) {
-                var doc = editor.getDoc();
-                var token = editor.getTokenAt(doc.getCursor());
-                console.log(token.state.context);
-                $rootScope.$apply(function () {
-                    $rootScope.$broadcast('hintslist', token.state.context.getAutocompletionsList())
-                })
-            })
+            editor.on('cursorActivity',updateContext);
+            editor.on('focus', updateContext);
+            editor.on('blur', clearContext);
         }
 
         return {
--- app/js/main.js
+++ app/js/main.js
@@ -12,14 +12,6 @@ require.config({
     ]
 });
 
-var tmpTest = "Title: Проверка примитива AddChoice\n" +
-    "TestName:;\n" +
-    "Difficulty: A1;\n" +
-    "FullTime: 0;\n" +
-    "Questions: 1;\n" +
-    "EndTitle.\n";
-
-
 require(['codemirror', 'tml-mode', 'app'], function (CodeMirror, tmlMode, app) {
 
     angular.element(document).ready(function() {
